e778b4d684a252d146a1705d291748ba9051a853
Sths <stevenhtx1993@163.com>
2014-01-22 15:42:14 +0800

    2 bad

---
+++ test/bad/c_cpp/MLE(more than 100MB).cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <cstring>
+
+
+int a[100000000];
+int main() {
+
+	return 0;
+}
---
+++ test/bad/c_cpp/MLE(unlimited increase when runtime).cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <cstdlib>
+int main() {
+	while (1) {
+		int * a = (int *) malloc(1000000 * sizeof(int));
+	}
+	return 0;
+}
